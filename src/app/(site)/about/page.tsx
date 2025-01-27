2680b93d70de021b39c200e908011d421496a293
af7slr77 <af7slr722@gmail.com>
2025-01-27 17:54:31 +0500

    refactored /src/app/(site)/about/page.tsx

--- src/app/(site)/about/page.tsx
+++ src/app/(site)/about/page.tsx
@@ -1,12 +1,10 @@
 import AboutSection from "@/components/About/AboutSection";
 import Team from "@/components/About/Team";
-import Video from "@/components/About/Video";
 import Breadcrumb from "@/components/Breadcrumb";
-import CallToAction from "@/components/CallToAction";
 import Clients from "@/components/Home/Clients";
 import Features from "@/components/Home/Features";
-import Reviews from "@/components/Home/Reviews";
-import Newsletter from "@/components/Newsletter";
+// import Reviews from "@/components/Home/Reviews";
+// import Newsletter from "@/components/Newsletter";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
